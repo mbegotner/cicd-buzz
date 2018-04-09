746c9b35dd65779fd4cfa6e8c1c9dd3d63b687ac
mbegotner <38075621+mbegotner@users.noreply.github.com>
2018-04-09 18:42:52 -0500

    testing heroku deployment

--- .travis/deploy_heroku.sh
+++ .travis/deploy_heroku.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh
 heroku plugins:install heroku-container-registry
-docker login -e _ -u _ --password=$HEROKU_API_KEY registry.heroku.com
-heroku container:push web --app $HEROKU_APP_NAME
+docker login -e _ -u _ --password='303effeb-fe4e-47f1-b712-c1fcd96f5d14' registry.heroku.com
+heroku container:push web --app 'intense-mesa-75551'
