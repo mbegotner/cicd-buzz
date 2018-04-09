884253937bce33d18d466d562341685153440384
mbegotner <38075621+mbegotner@users.noreply.github.com>
2018-04-09 11:48:00 -0500

    testing fix

--- buzz/generator.py
+++ buzz/generator.py
@@ -11,11 +11,11 @@ def sample(l, n = 1):
         return result[0]
     return result
 
-#def generate_buzz():
-#    buzz_terms = sample(buzz, 2)
-#    phrase = ' '.join([sample(adjectives), buzz_terms[0], sample(adverbs),
-#       sample(verbs), buzz_terms[1]])
-#    return phrase.title()
+def generate_buzz():
+    buzz_terms = sample(buzz, 2)
+    phrase = ' '.join([sample(adjectives), buzz_terms[0], sample(adverbs),
+       sample(verbs), buzz_terms[1]])
+    return phrase.title()
 
 if __name__ == "__main__":
     print(generate_buzz())
